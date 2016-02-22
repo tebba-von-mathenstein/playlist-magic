b28c7be8729de56e416ec1fdf93ff9ea772e05dd
Tyler Bettilyon <teb311@gmail.com>
2016-02-21 17:00:19 -0800

    successfully getting the discography for playlists so long as we do not get rate limited

--- client/js/services/SpotifyRequester.js
+++ client/js/services/SpotifyRequester.js
@@ -1,86 +1,116 @@
 var app = angular.module("PlaylistMagic");
 
 app.factory("SpotifyRequester", function($http) {
-    var SpotifyRequester = {
+    /**
+     * Given a spotify artist url begin a series of requests that
+     * ultimately fetches all the albums with their track list for
+     * that artist.
+     */
+    function albumsAndTracks(artistId) {
+        var artistUrl = 'https://api.spotify.com/v1/artists/' + artistId;
+        return $http({
+            url: artistUrl + '/albums',
+            method: "GET",
+        })
+        .then(function(albumsResponse){
+            var albumsData = albumsResponse.data;
+
+            albumsData = albumsData.items.map(function(albumObj) {
+                return {
+                    name: albumObj.name,
+                    url: albumObj.href
+                }
+            });
+
+            var albumPromises = [];
+            for(i in albumsData){
+                var curAlbum = albumsData[i];
+                var curAlbumPromise = $http({
+                    url: curAlbum.url,
+                    method: "GET"
+                });
+
+                albumPromises.push(curAlbumPromise);
+            }
+
+            return Promise.all(albumPromises);
+        })
+        .then(function(resolvedAlbums){
+            var expandedAlbumData = resolvedAlbums.map(function(albumResponse) {
+                var obj = albumResponse.data;
+                return {
+                    name: obj.name,
+                    releaseDate: obj.release_date,
+                    tracks: obj.tracks
+                }
+            });
+
+            return expandedAlbumData;
+        })
+        .catch(function(error) {
+            console.log(error);
+            return error;
+        });
+    }
+
+    /**
+     * Given a userId and a playlist -- collect all the artists who appear
+     * on the playlist, then generate their discography. Return an object
+     * that the discographies.
+     */
+    function discographyPlaylist(userId, playlistId) {
+        var playlistTracksUrl = 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks'
         
-        /**
-         * Given a spotify artist url begin a series of requests that
-         * ultimately fetches all the albums with their track list for
-         * that artist.
-         */
-        albumsAndTracks: function albumsAndTracks(artistId) {
-            var artistUrl = 'https://api.spotify.com/v1/artists/' + artistId;
-            $http({
-                url: artistUrl + '/albums',
-                method: "GET",
-            })
-            .then(function(albumsResponse){
-                var albumsData = JSON.parse(albumsResponse.body);
+        $http({
+            url: playlistTracksUrl,
+            method: "GET"
+        }).then(function(tracksResp) {
+            var tracksPromises = [tracksResp];
+            console.log(tracksResp.data);
 
-                albumsData = albumsData.items.map(function(albumObj) {
-                    return {
-                        name: albumObj.name,
-                        url: albumObj.href
-                    }
-                });
-
-                var albumPromises = [];
-                for(i in albumsData){
-                    var curAlbum = albumsData[i];
-                    var curAlbumPromise = $http({
-                        url: curAlbum.url,
+            // Spotify API limit is 100 -- we want all tracks so
+            if(tracksResp.data.total > 100) {
+                for(var i = 99; i < tracksResp.data.total; i += 100) {
+                    var extraTracksUrl = 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks?offset=' + i;
+                    tracksPromises.push($http({
+                        url: extraTracksUrl,
                         method: "GET"
-                    });
-
-                    albumPromises.push(curAlbumPromise);
+                    }));
                 }
+            }
+            return Promise.all(tracksPromises);
+        }).then(function(allTrackResponses){
+            console.log(allTrackResponses);
 
-                return Promise.all(albumPromises);
-            })
-            .then(function(resolvedAlbums){
-                var expandedAlbumData = resolvedAlbums.map(function(albumResponse) {
-                    var obj = JSON.parse(albumResponse.body);
-                    return {
-                        name: obj.name,
-                        releaseDate: obj.release_date,
-                        tracks: obj.tracks
-                    }
-                });
+            var artistsOnly = {};
+            for(var respI in allTrackResponses) {
 
-                return expandedAlbumData;
-            })
-            .catch(function(error) {
-                console.log(error);
-                return error;
-            });
-        },
+                var response = allTrackResponses[respI];
+                for(var trackI in response.data.items) {
+                    
+                    var track = response.data.items[trackI].track;
+                    for(var artI in track.artists) {
 
-        discographyPlaylist: function(userId, playlistId) {
-            var playlistTracksUrl = 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks'
-            
-            $http({
-                url: playlistTracksUrl,
-                method: "GET"
-            }).then(function(tracksResp) {
-                var tracksPromises = [tracksResp];
-                console.log(tracksResp.data);
-
-                // Spotify API limit is 100 -- we want all tracks so
-                if(tracksResp.data.total > 100) {
-                    for(var i = 99; i < tracksResp.data.total; i += 100) {
-                        var extraTracksUrl = 'https://api.spotify.com/v1/users/' + userId + '/playlists/' + playlistId + '/tracks?offset=' + i;
-                        tracksPromises.push($http({
-                            url: extraTracksUrl,
-                            method: "GET"
-                        }));
+                        var artist = track.artists[artI];
+                        if(artist.id) artistsOnly[artist.id] = artist.name;
                     }
                 }
-                return Promise.all(tracksPromises);
-            }).then(function(allTrackResponses){
-                console.log(allTrackResponses);
-            });
-        }
+            }
+
+            var allSongsPromises = [];
+            for(artistId in artistsOnly) {
+                allSongsPromises.push(albumsAndTracks(artistId));
+            }
+
+            return Promise.all(allSongsPromises)
+        }).then(function(discographyResponses){
+            console.log(discographyResponses);
+        });
     };
 
+    var SpotifyRequester = {
+        albumsAndTracks: albumsAndTracks,
+        discographyPlaylist: discographyPlaylist
+    }
     return SpotifyRequester;
 });
